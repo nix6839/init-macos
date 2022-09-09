f9f9a43f76b6fb3be4746e223f5cfa6f8cc008e9
Han Yeong-woo <nix6839@washnix.com>
2022-09-09 19:28:27 +0900

    feat: install autopep8 and pylint

--- internal/install/python.go
+++ internal/install/python.go
@@ -5,11 +5,24 @@ import (
 	"github.com/nix6839/macos-init/pkg/utils"
 )
 
+var pythonPackages = []string{
+	"autopep8", "pylint",
+}
+
 func fetchPoetryScript() string {
 	return utils.FetchBody("https://install.python-poetry.org")
 }
 
 func Python() {
+	command.New("pip3").
+		Arg("install").
+		Arg("--upgrade").
+		Arg("pip")
+
+	command.New("pip3").
+		Arg("install").
+		Args(pythonPackages)
+
 	command.New("python3").
 		Arg("-c").
 		Arg(fetchPoetryScript()).
